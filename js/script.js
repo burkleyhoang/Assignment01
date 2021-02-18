e2facf6431cbe05e588fc9587deffb68c034934a
Burkley Hoang <67705100+burkleyhoang@users.noreply.github.com>
2021-02-17 19:42:16 -0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// STEP 1
+// let someMonth;
+// function theMonth;
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// STEP 2 ****** missing Null
+// 0x1123 - numeric literal
+// "Mike's drink" - string literal
+// 5 = 5 - Boolean literal
+//         - null literal
+
+// STEP 3
+// let total = 5 + (6 * 8); - complex
+// var name = "Burkley"; - variable
+
+// STEP 4
+// var firstName;
+// var lastName;
+// var address;
+// var city;
+// var state;
+// var zipCode;
+// var yourAge;
+// var referralSource;
+// var mayWeContactYou;
+
+// STEP 5
+// var firstName = "Mike";
+// var lastName = "Jones";
+// var city = "San Diego";
+
+// STEP 6
+
+
