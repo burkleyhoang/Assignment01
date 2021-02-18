a03d42d78e22f340efe5ed80d6108861d8f4de45
Burkley Hoang <67705100+burkleyhoang@users.noreply.github.com>
2021-02-18 10:58:27 -0800

    https://github.com/burkleyhoang/Assignment01.git

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@
 // let summerMonth;
 // let myLibraryFunction;
 
-// STEP 2 ****** missing Null
-// 0x1123 - numeric literal
-// "Mike's drink" - string literal
-// 5 = 5 - Boolean literal
-//         - null literal
+// STEP 2 
+// var num = 0x1123; 
+// var string = "Mike's drink";
+// var bLean = 5 = 5;
+// var y = null;
 
 // STEP 3
-// let total = 5 + (6 * 8); - complex
-// var name = "Burkley"; - variable
+// let total = 5 + (6 * 8);
+// var name = "Burkley";
 
 // STEP 4
 // var firstName;
@@ -32,5 +32,74 @@
 // var city = "San Diego";
 
 // STEP 6
+// let speedLimit = 55; 
+// console.log("The highway speedlimit is " + speedLimit);
+
+// STEP 7
+// let x = 10;
+// let y = 10;
+// alert(x == y);
 
 
+
+// STEP 8
+// let someString = "Who once said, \" Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\""; 
+// console.log(someString);
+
+// STEP 9
+// let nope = null;
+// console.log(nope);
+// let gone = undefined;
+// console.log(gone);
+
+// STEP 10
+// console.log(typeof "Name");
+// console.log(typeof 0x425);
+// console.log(typeof false);
+// console.log(typeof null);
+// console.log(typeof bye);
+
+
+
+// STEP 11
+// alert("Hello " + "Burkley Hoang" + ", welcome to the JavaScript class!");
+
+// STEP 12
+// var name = "Burkley";
+// alert(name);
+
+// STEP 13
+// var course = "JavaScript";
+// alert("JavaScript");
+
+// STEP 14
+// alert("Hello Zak Ruvalcaba.\nWelcome to the JavaScript class!");
+
+// STEP 15
+// var name = "Burkley";
+// name = "What's your name";
+// prompt(name);
+
+// STEP 16
+// var course = "FEWD 644";
+// course = "What class are you taking?";
+// prompt(course);
+
+// STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
+// STEP 18
+// let x = 20;
+// x = 20 + 20;
+// console.log(x);
+
+// STEP 19
+// let x = 20;
+// x = x * 5;
+// console.log(x);
+
+// STEP 21
+
+// STEP 22
